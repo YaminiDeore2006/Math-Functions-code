49dc365f4c53fb2459763189a65675185de8c3f2
G1tHubYami2006 <yaminideore2006@gmail.com>
2026-05-29 23:54:12 +0530

    initial commit

---
+++ PrimeCheck.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;   // 0 and 1 are not prime
+        if (n <= 3) return true;    // 2 and 3 are prime
+
+        // eliminate multiples of 2 and 3
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        // check from 5 to sqrt(n)
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        if (isPrime(n))
+            System.out.println("Prime Number");
+        else
+            System.out.println("Not a Prime Number");
+
+        sc.close();
+    }
+}
---
+++ sum_of_naturals.java
@@ -0,0 +1,8 @@
+public class sum_of_naturals
+{
+    public static void main(String[] args)
+    {
+        int n = 5 ;
+        System.out.println(n * (n + 1) / 2);
+    }
+}
